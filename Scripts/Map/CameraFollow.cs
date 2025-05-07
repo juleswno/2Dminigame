1770709899a44c007411f4f83fa3d6d9558bf44f
juleswno <reve0107@gmail.com>
2025-05-07 14:29:41 +0900

    Update CameraFollow.cs

--- Scripts/Map/CameraFollow.cs
+++ Scripts/Map/CameraFollow.cs
@@ -1,1 +1,27 @@
+using UnityEngine;
 
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 5f;
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desired = target.position + offset;
+        desired.z = transform.position.z;
+
+        desired.x = Mathf.Clamp(desired.x, minBounds.x, maxBounds.x);
+        desired.y = Mathf.Clamp(desired.y, minBounds.y, maxBounds.y);
+
+        transform.position = Vector3.Lerp(transform.position, desired, Time.deltaTime * smoothSpeed);
+    }
+}
