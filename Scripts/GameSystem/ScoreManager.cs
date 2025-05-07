770f04c310cb897950c4cc129799e986b3651e51
juleswno <reve0107@gmail.com>
2025-05-07 14:30:30 +0900

    Update ScoreManager.cs

--- Scripts/GameSystem/ScoreManager.cs
+++ Scripts/GameSystem/ScoreManager.cs
@@ -1,1 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
 
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance { get; private set; }
+    public int score = 0;
+    public Text scoreText;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScoreUI();
+    }
+
+    public void SaveHighScore()
+    {
+        int best = PlayerPrefs.GetInt("HighScore", 0);
+        if (score > best)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private void UpdateScoreUI()
+    {
+        if (scoreText != null)
+            scoreText.text = $"점수: {score}";
+    }
+}
