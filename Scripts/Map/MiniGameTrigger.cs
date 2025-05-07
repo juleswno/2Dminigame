120cecb56b20c417b30b113dc51093fd075efc31
juleswno <reve0107@gmail.com>
2025-05-07 14:30:04 +0900

    Update MiniGameTrigger.cs

--- Scripts/Map/MiniGameTrigger.cs
+++ Scripts/Map/MiniGameTrigger.cs
@@ -1,1 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public class MiniGameTrigger : MonoBehaviour
+{
+    public GameObject popupUI;
+    public string sceneToLoad = "FlappyGame";
+    private bool canEnter = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popupUI.SetActive(true);
+            canEnter = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popupUI.SetActive(false);
+            canEnter = false;
+        }
+    }
+
+    void Update()
+    {
+        if (canEnter && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
